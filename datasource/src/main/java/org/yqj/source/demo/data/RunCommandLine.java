2fc575067a8489319e1331801da7063c98da7b76
qijunyao <qijunyao@xiaohongshu.com>
2021-04-04 02:11:35 +0800

    add

---
+++ datasource/src/main/java/org/yqj/source/demo/data/RunCommandLine.java
@@ -0,0 +1,27 @@
+package org.yqj.source.demo.data;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Description:
+ *
+ * @author yaoqijun
+ * @date 2018/8/15
+ * Email: yaoqijunmail@foxmail.com
+ */
+@Component
+@Slf4j
+public class RunCommandLine implements CommandLineRunner{
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("starting command line ...");
+    }
+}
