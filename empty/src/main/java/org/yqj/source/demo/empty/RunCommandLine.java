d6a4aa27cc7d9fe6aad8e3bf876dbba8892408de
qijunyao <qijunyao@xiaohongshu.com>
2021-04-26 17:36:36 +0800

    add all

--- empty/src/main/java/org/yqj/source/demo/empty/RunCommandLine.java
+++ empty/src/main/java/org/yqj/source/demo/empty/RunCommandLine.java
@@ -4,11 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
 /**
  * Description:
  *
