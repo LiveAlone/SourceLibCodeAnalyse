7237dd9a49d6bc7abf72cd2c291243b7efebc492
qijunyao <qijunyao@xiaohongshu.com>
2021-07-05 14:00:40 +0800

    add all

---
+++ lettuce/src/test/java/org/yqj/source/demo/lettuce/JedisBasicTest.java
@@ -0,0 +1,30 @@
+package org.yqj.source.demo.lettuce;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Ignore;
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+/**
+ * Description:
+ *
+ * @author yaoqijun
+ * @date 2021/7/5
+ * Email: yaoqijunmail@foxmail.com
+ */
+@Slf4j
+public class JedisBasicTest {
+
+    @Test
+    public void basicTest() {
+        Jedis jedis = new Jedis("localhost", 6379);
+//        for (int i = 0; i < 100; i++) {
+//            jedis.zadd("testSet", i, String.valueOf(i));
+//        }
+        System.out.println(jedis.zrank("testSet", "1024"));
+        System.out.println(jedis.zrank("testSet", "66"));
+        System.out.println(jedis.zrank("testSet", "100"));
+        System.out.println(jedis.zrank("testSet", "0"));
+    }
+
+}
