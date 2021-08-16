e77a093aa64d5e7ec8e1a1c0e5b3d05ed7040228
qijunyao <qijunyao@xiaohongshu.com>
2021-08-16 20:42:59 +0800

    add all

--- empty/build.gradle.kts
+++ empty/build.gradle.kts
@@ -4,8 +4,8 @@
 
 plugins {
     id("org.yqj.source.java-spring-conventions")
-    id("org.springframework.boot") version ("2.5.3")
-    id("io.spring.dependency-management") version ("1.0.11.RELEASE")
+    id("org.springframework.boot") version "2.5.3"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 repositories {
